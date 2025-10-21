d55081a57272ed674e52cd3c9c5ac932948e941a
Alejandro Melo <daniel.melo@sinco.com.co>
2025-10-21 12:11:57 -0500

    🟥 Si_enviamos_comando_R_debe_retornar_coordenada_OOE

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -11,6 +11,18 @@ public class MarsRoverTest
         resultado.Should().Be("0:0:N");
     }
 
+    [Fact]
+    public void Si_enviamos_comando_R_debe_retornar_coordenada_OOE()
+    {
+        var resultado = Girar("R");
+        resultado.Should().Be("0:0:E");
+    }
+
+    private object Girar(string s)
+    {
+        throw new NotImplementedException();
+    }
+
     private string ObtenerCoordenadaInicial()
     {
         return "0:0:N";
