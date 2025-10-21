26df4598e403feddaa9320a43a6f1610cca6a86d
Alejandro Melo <daniel.melo@sinco.com.co>
2025-10-21 14:57:33 -0500

    🟥 realizar un giro a la derecha

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -7,9 +7,27 @@ public class MarsRoverTest
     [Fact]
     public void Debe_obtener_ubicacion_inicial()
     {
+        // Arrange
         var ubicacion = new Ubicacion();
-        ubicacion.ObtenerCoordernada().Should().Be("0:0:N");
+        
+        // Act
+        var coordenada= ubicacion.ObtenerCoordernada();
+        
+        // Assert
+        coordenada.Should().Be("0:0:N");
     }
+
+    [Fact]
+    public void Si_envio_comando_R_debe_retonar_OOE()
+    {
+        // Arrange
+        var ubicacion = new Ubicacion();
+        // Act
+        var coordenada= ubicacion.RealizarGiro('R');
+        // Assert
+        coordenada.Should().Be("0:0:E");
+    }
+    
 }
 
 public class Ubicacion
@@ -19,5 +37,10 @@ public class Ubicacion
     public string ObtenerCoordernada()
     {
         return CoordenadaInicial;
+    }
+
+    public object RealizarGiro(char c)
+    {
+        throw new NotImplementedException();
     }
 }
