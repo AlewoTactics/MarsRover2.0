881385ff2671df6c73bf29d5de52f8bc25c57a80
Alejandro Melo <daniel.melo@sinco.com.co>
2025-10-21 16:09:21 -0500

    🟦 Utilidad de giros ilimitados

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -17,89 +17,38 @@ public class MarsRoverTest
         coordenada.Should().Be("0:0:N");
     }
 
-    [Fact]
-    public void Si_envio_comando_R_debe_retonar_OOE()
+    [Theory]
+    [InlineData(1, "0:0:E")]
+    [InlineData(2, "0:0:S")]
+    [InlineData(3, "0:0:W")]
+    [InlineData(4, "0:0:N")]
+    [InlineData(5, "0:0:E")]
+    [InlineData(10, "0:0:S")]
+    public void Si_giro_n_veces_a_la_derecha_desde_el_norte_debe_retornar_la_cordenada_correspondiente(
+        int cantidadGiros, string orientacionEsperada)
     {
-        // Arrange
-        var ubicacion = new Ubicacion();
-        // Act
-        ubicacion.RealizarGiro('R');
-        // Assert
-        ubicacion.ObtenerCoordernada().Should().Be("0:0:E");
+        //Arrange
+        var utilidadesUbicacion = new Utilidades();
+        //act
+        utilidadesUbicacion.RealizarGiros(cantidadGiros);
+
+        //assert
+        utilidadesUbicacion.Ubicacion.ObtenerCoordernada().Should().Be(orientacionEsperada);
     }
-
-    [Fact]
-    public void Si_estoy_en_el_Este_al_girar_a_la_derecha_debe_estar_en_la_coordenada_00S()
-    {
-        // Arrange
-        var ubicacion = new Ubicacion();
-        ubicacion.RealizarGiro('R');
-
-        // Act
-        ubicacion.RealizarGiro('R');
-        // Assert
-        ubicacion.ObtenerCoordernada().Should().Be("0:0:S");
-    }
-
-    [Fact]
-    public void Si_estoy_en_el_Sur_al_girar_a_la_derecha_debe_estar_en_la_coordenada_00W()
-    {
-        // Arrange
-        var ubicacion = new Ubicacion();
-        ubicacion.RealizarGiro('R');
-        ubicacion.RealizarGiro('R');
-
-        // Act
-        ubicacion.RealizarGiro('R');
-        // Assert
-        ubicacion.ObtenerCoordernada().Should().Be("0:0:W");
-    }
-
-    [Fact]
-    public void Si_estoy_en_el_oeste_y_giro_derecha_debe_estar_en_la_coordenada_00N()
-    {
-        // Arrange
-        var ubicacion = new Ubicacion();
-        ubicacion.RealizarGiro('R');
-        ubicacion.RealizarGiro('R');
-        ubicacion.RealizarGiro('R');
-
-        // Act
-        ubicacion.RealizarGiro('R');
-        // Assert
-        ubicacion.ObtenerCoordernada().Should().Be("0:0:N");
-        
-    }
-
+    
+    
+    
 }
 
-public class Ubicacion
+public class Utilidades
 {
-    const string CoordenadaInicial = "0:0:";
-    private char Orientacion { get; set; }
 
-    public Ubicacion()
+    public readonly Ubicacion Ubicacion = new();
+    public void RealizarGiros(int numeroGiros)
     {
-        Orientacion = 'N';
-    }
-
-    public string ObtenerCoordernada()
-    {
-        return CoordenadaInicial + Orientacion;
-    }
-
-    public void RealizarGiro(char comando)
-    {
-        if (comando == 'R')
+        for (var iteracion = 0; iteracion < numeroGiros; iteracion++)
         {
-            Orientacion = Orientacion switch
-            {
-                'N' => 'E',
-                'E' => 'S',
-                'S' => 'W',
-                'W' => 'N',
-                _ => Orientacion
-            };
+            Ubicacion.RealizarGiro('R');
         }
     }
 }
---
+++ MarsRovers/Ubicacion.cs
@@ -0,0 +1,32 @@
+﻿namespace MarsRovers;
+
+public class Ubicacion
+{
+    const string CoordenadaInicial = "0:0:";
+    private char Orientacion { get; set; }
+
+    public Ubicacion()
+    {
+        Orientacion = 'N';
+    }
+
+    public string ObtenerCoordernada()
+    {
+        return CoordenadaInicial + Orientacion;
+    }
+
+    public void RealizarGiro(char comando)
+    {
+        if (comando == 'R')
+        {
+            Orientacion = Orientacion switch
+            {
+                'N' => 'E',
+                'E' => 'S',
+                'S' => 'W',
+                'W' => 'N',
+                _ => Orientacion
+            };
+        }
+    }
+}
