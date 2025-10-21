c78f78d39f71d2264f3e61fa806aa8809a21c3d1
Alejandro Melo <daniel.melo@sinco.com.co>
2025-10-21 12:04:08 -0500

    🟦 Si_enviamos_comando_vacio_debe_retornar_coordena_00N

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -2,16 +2,16 @@
 
 namespace MarsRovers;
 
-public class UnitTest1
+public class MarsRoverTest
 {
     [Fact]
     public void Si_enviamos_comando_vacio_debe_retornar_coordena_00N()
     {
-        var resultado = ObtenerCoordenadaInicial("");
+        var resultado = ObtenerCoordenadaInicial();
         resultado.Should().Be("0:0:N");
     }
 
-    private object ObtenerCoordenadaInicial(string empty)
+    private string ObtenerCoordenadaInicial()
     {
         return "0:0:N";
     }
