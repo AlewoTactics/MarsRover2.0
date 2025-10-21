352a9dd06fab7ed26451cb5ed939d40002e7ece2
Alejandro Melo <daniel.melo@sinco.com.co>
2025-10-21 14:39:38 -0500

    🟥 Creacion del objeto ubicacion para obtener coordenas actuales

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -5,47 +5,20 @@ namespace MarsRovers;
 public class MarsRoverTest
 {
     [Fact]
-    public void Si_enviamos_comando_vacio_debe_retornar_coordena_00N()
+    public void Debe_obtener_ubicacion_inicial()
     {
-        var resultado = ObtenerCoordenadaInicial();
-        resultado.Should().Be("0:0:N");
+        var ubicacion = new Ubicacion();
+        ubicacion.ObtenerCoordernada().Should().Be("0:0:N");
     }
+    
+}
 
-    [Fact]
-    public void Si_enviamos_comando_R_debe_retornar_coordenada_OOE()
+public class Ubicacion
+{
+    public string ObtenerCoordernada()
     {
-        var resultado = Girar("R");
-        resultado.Should().Be("0:0:E");
-    }
-
-    [Fact]
-    public void Si_enviamos_comando_L_debe_retornar_coordenada_OOW_desde_la_posicion_inicial()
-    {
-        var resultado = Girar("L");
-        resultado.Should().Be("0:0:W");
-    }
-
-    [Fact]
-    public void Si_enviamos_un_comando_con_LL_debe_retornar_coordenada_00S_desde_la_posicion_inicial()
-    {
-        var resultado = Girar("LL");
-        resultado.Should().Be("0:0:S");
-    }
-
-    private string Girar(string comando)
-    {
-        var posicionInicial = ObtenerCoordenadaInicial();
-        return comando switch
-        {
-            "R" => posicionInicial.Replace("N", "E"),
-            "L" => posicionInicial.Replace("N", "W"),
-            "LL" => posicionInicial.Replace("N", "S"),
-            _ => posicionInicial
-        };
-    }
-
-    private string ObtenerCoordenadaInicial()
-    {
-        return "0:0:N";
+        throw new NotImplementedException();
     }
 }
+
+
