0f8abd46333a5f4980bd44a78bf0090cedc2c974
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-22 10:28:10 -0500

    🟥 Si_ElComandoEsVacio_Debe_Retornar00N

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -5,82 +5,19 @@ namespace MarsRovers;
 public class MarsRoverTest
 {
     [Fact]
-    public void Debe_obtener_ubicacion_inicial()
+    public void Si_ElComandoEsVacio_Debe_Retornar00N()
     {
-        // Arrange
-        var ubicacion = new Ubicacion();
-
-        // Act
-        var coordenada = ubicacion.ObtenerCoordernada();
-
-        // Assert
-        coordenada.Should().Be("0:0:N");
+        var marsRover = new MarsRover();
+        string  ubicacion = marsRover.EjecutarComando("");
+        ubicacion.Should().Be("0:0:N");
     }
-
-    [Theory]
-    [InlineData(1, "0:0:E")]
-    [InlineData(2, "0:0:S")]
-    [InlineData(3, "0:0:W")]
-    [InlineData(4, "0:0:N")]
-    [InlineData(5, "0:0:E")]
-    [InlineData(10, "0:0:S")]
-    public void Si_giro_n_veces_a_la_derecha_desde_el_norte_debe_retornar_la_cordenada_correspondiente(
-        int cantidadGiros, string orientacionEsperada)
-    {
-        //Arrange
-        var utilidadesUbicacion = new Utilidades();
-        //act
-        utilidadesUbicacion.RealizarGirosDerecha(cantidadGiros);
-
-        //assert
-        utilidadesUbicacion.ObtenerUbicacion().Should().Be(orientacionEsperada);
-    }
-
-    [Theory]
-    [InlineData(1, "0:0:W")]
-    [InlineData(2, "0:0:S")]
-    [InlineData(3, "0:0:E")]
-    [InlineData(4, "0:0:N")]
-    [InlineData(5, "0:0:W")]
-    [InlineData(10, "0:0:S")]
-    public void Si_giro_N_veces_a_la_izquierda_debe_retornar_la_coordenada_correspondiente(int cantidadGiros, string orientacionEsperada)
-    {
-        //Arrange
-        var utilidadesUbicacion = new Utilidades();
-        //act
-        utilidadesUbicacion.RealizarGirosIzquierda(cantidadGiros);
-
-        //assert
-        utilidadesUbicacion.ObtenerUbicacion().Should().Be(orientacionEsperada);
-    }
-    
 }
 
-public class Utilidades
+public class MarsRover
 {
-    private readonly Ubicacion Ubicacion = new();
-
-    public void RealizarGirosDerecha(int numeroGiros)
+    public string EjecutarComando(string empty)
     {
-        RealizarGiro(numeroGiros, Giro.Derecha);
-
-    }
-
-    public void RealizarGirosIzquierda(int numeroGiros)
-    {
-        RealizarGiro(numeroGiros, Giro.Izquierda);
-    }
-
-    private void RealizarGiro(int numeroGiros, Giro giro)
-    {
-        for (var iteracion = 0; iteracion < numeroGiros; iteracion++)
-        {
-            Ubicacion.RealizarGiro(giro);
-        }
-    }
-
-    public string ObtenerUbicacion()
-    {
-        return Ubicacion.ObtenerCoordernada();
+        
+        throw new NotImplementedException();
     }
 }
