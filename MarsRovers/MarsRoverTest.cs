b003507ff7f26e5cdc4722bdf0e9786545fed0eb
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-22 12:46:47 -0500

    🟥 Si_OrientacionEsOesteYGiroALaIzquierda_Debe_OrientarseAlSurEnCoordenada00S

--- MarsRovers/MarsRoverTest.cs
+++ MarsRovers/MarsRoverTest.cs
@@ -16,7 +16,7 @@ public class MarsRoverTest
     public void Si_OrientacionEsEsteYGiroAlaDerecha_Debe_OrientarseAlEsteEnCoordenada00S()
     {
         //ARRANGE
-        var marsRover = new MarsRover(); 
+        var marsRover = new MarsRover();
         marsRover.EjecutarComando("R");
         //ACT
         string ubicacion = marsRover.EjecutarComando("R");
@@ -28,7 +28,7 @@ public class MarsRoverTest
     public void Si_OrientacionEsSurYGiroALaDerecha_Debe_OrientarseAlOesteEnCordenada00W()
     {
         //ARRANGE
-        var marsRover = new MarsRover(); 
+        var marsRover = new MarsRover();
         marsRover.EjecutarComando("R");
         marsRover.EjecutarComando("R");
         //ACT
@@ -36,19 +36,19 @@ public class MarsRoverTest
         //ASSERT
         ubicacion.Should().Be("0:0:W");
     }
-    
+
     [Fact]
     public void Si_OrientacionEsOesteYGiroALaDerecha_Debe_OrientarseAlNorteEnCordenada00N()
     {
         //ARRANGE
-        var marsRover = new MarsRover(); 
+        var marsRover = new MarsRover();
         marsRover.EjecutarComando("R");
         marsRover.EjecutarComando("R");
         marsRover.EjecutarComando("R");
-        
+
         //ACT
         string ubicacion = marsRover.EjecutarComando("R");
-        
+
         //ASSERT
         ubicacion.Should().Be("0:0:N");
     }
@@ -57,12 +57,25 @@ public class MarsRoverTest
     public void Si_OrientacionEsNorteYGiroAlaIzquierda_Debe_OrientarseAlEsteEnCoordenada00W()
     {
         //ARRANGE
-        var marsRover = new MarsRover(); 
-        
+        var marsRover = new MarsRover();
+
         //ACT
         string ubicacion = marsRover.EjecutarComando("L");
-        
+
         //ASSERT
         ubicacion.Should().Be("0:0:W");
+    }
+
+    [Fact]
+    public void Si_OrientacionEsOesteYGiroALaIzquierda_Debe_OrientarseAlSurEnCoordenada00S()
+    {
+        //ARRANGE
+        var marsRover = new MarsRover();
+        marsRover.EjecutarComando("L");
+        //ACT
+        string ubicacion = marsRover.EjecutarComando("L");
+
+        //ASSERT
+        ubicacion.Should().Be("0:0:S");
     }
 }
