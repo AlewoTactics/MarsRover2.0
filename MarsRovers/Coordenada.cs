d06307ea3113455443542098c6375772346490a4
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-22 17:24:04 -0500

    ✅ Si_SuperaElLimiteEnYHaciaElSur_Debe_RetornarLaCordenanda09S

--- MarsRovers/Coordenada.cs
+++ MarsRovers/Coordenada.cs
@@ -2,19 +2,20 @@
 
 public class Coordenada(int x, int y)
 {
-
     public override string ToString() => $"{x}:{y}";
 
     public Coordenada IrAlNorte()
     {
         if (y == 9)
             return new Coordenada(x, 0);
-        
+
         return new Coordenada(x, y + 1);
     }
 
     public Coordenada IrAlSur()
     {
+        if (y == 0)
+            return new Coordenada(x, 9);
         return new Coordenada(x, y - 1);
     }
 
