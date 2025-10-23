5f1c05280a2901449cf11063587ce8e19dd1ba18
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-23 14:56:45 -0500

    🔴Si_RecibeUnaCoordernadaMenorDelLimiteX_Debe_LanzarUnaExcepcion

--- MarsRovers/CoordenadaTest.cs
+++ MarsRovers/CoordenadaTest.cs
@@ -6,12 +6,20 @@ public class CoordenadaTest
 {
  
     [Fact]
-    public void Si_RecibeUnaCoordernadaFueraDelLimite_Debe_LazarUnaExcepcion()
+    public void Si_RecibeUnaCoordernadaMayorDelLimiteX_Debe_LanzarUnaExcepcion()
     {
         Action coordenada =()=> new Coordenada(12, 0);
 
         coordenada.Should()
             .Throw<ArgumentOutOfRangeException>().WithMessage("*La coordenada en x (12) supera el límite máximo (9)*"); 
+    }
 
+    [Fact]
+    public void Si_RecibeUnaCoordernadaMenorDelLimiteX_Debe_LanzarUnaExcepcion()
+    {
+        Action coordenada =()=> new Coordenada(-1, 0);
+        
+        coordenada.Should()
+            .Throw<ArgumentOutOfRangeException>().WithMessage("*La coordenada en x (-1) no se encuentra en el rango de la plataforma*"); 
     }
 }
